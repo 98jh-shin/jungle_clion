9f43fa6d5dfd2d80e40f6a6257cb6f53d8399644
shinjungho <efsxr879@gmail.com>
2025-08-13 14:42:03 +0900

    BST 1번 BFS로 해결 완료

--- Data-Structures/Binary_Search_Tree/Q1_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q1_F_BST.c
@@ -93,8 +93,24 @@ int main()
 
 void levelOrderTraversal(BSTNode* root)
 {
+    if (root == NULL) return;
 
-    /* add your code here */
+    Queue queue;
+    queue.head = NULL;
+    queue.tail = NULL;
+
+    enqueue(&queue.head, &queue.tail, root);
+
+    while (!isEmpty(queue.head)) {
+        BSTNode *node = dequeue(&queue.head, &queue.tail);
+        printf("%d ", node->item);
+        if (node->left != NULL) {
+            enqueue(&queue.head, &queue.tail, node->left);
+        }
+        if (node->right != NULL) {
+            enqueue(&queue.head, &queue.tail, node->right);
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
